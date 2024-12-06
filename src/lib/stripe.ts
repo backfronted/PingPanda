8afc6288c44fe2a7ce74924a4a07f74086924088
Karimov Firdavs <145204313+backfronted@users.noreply.github.com>
2024-12-06 20:38:39 +0500

    Update stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe"
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-  apiVersion: "2024-11-20.acacia",
+  apiVersion: "2024-09-30.acacia",
   typescript: true,
 })
 
@@ -15,7 +15,7 @@ export const createCheckoutSession = async ({
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
-        price: "price_1QBHVBA19umTXGu8gzhUCSG7",
+        price: "price_1QT3YNLZPfixIQ5gofbeO3Zo",
         quantity: 1,
       },
     ],
